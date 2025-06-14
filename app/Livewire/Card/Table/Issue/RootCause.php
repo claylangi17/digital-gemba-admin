d4a5162e273acb082700b289dd00aeb82aa3b8af
ardfar <farrasarrafi5b@gmail.com>
2025-06-14 22:31:26 +0700

    feat: added rott cause filtering

---
+++ app/Livewire/Card/Table/Issue/RootCause.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Livewire\Card\Table\Issue;
+
+use App\Models\RootCauses;
+use Livewire\Component;
+
+class RootCause extends Component
+{
+    public $root_causes;
+    public $issue;
+
+    public function mount($issue)
+    {
+        $this->issue = $issue;
+        $this->get_data();
+    }
+
+    public function change_category($category)
+    {
+        $this->get_data($category);
+
+        $this->dispatch('resetGenbaCauseTable');
+    }
+
+    public function get_data($category = 'all')
+    {
+        if ($category == 'all')
+        {
+            $this->root_causes = RootCauses::where('issue_id', $this->issue->id)->get();
+        } else {
+            $this->root_causes = RootCauses::where('issue_id', $this->issue->id)->where('category', $category)->get();
+        }
+    }
+    
+    public function render()
+    {
+        return view('livewire.card.table.issue.root-cause');
+    }
+}
--- resources/views/gemba/issue.blade.php
+++ resources/views/gemba/issue.blade.php
@@ -205,114 +205,7 @@
         </div>
         {{-- Files Card : End  --}}
 
-        {{-- Root Cause Card : Start  --}}
-        <div class="card h-full p-0 rounded-xl border-0 overflow-hidden w-full my-4">
-            <div class="card-header border-b border-neutral-200 dark:border-neutral-600 bg-white dark:bg-neutral-700 py-4 px-6 flex items-center flex-wrap gap-3 justify-between">
-                <div class="flex items-center flex-wrap gap-3">
-                    <span class="text-xl font-medium text-secondary-light mb-0">Akar Masalah </span>
-
-                    <button onclick="location.reload()" class="btn btn-secondary border border-neutral-600 text-sm btn-sm px-3 py-3 rounded-lg flex items-center gap-2">
-                        <iconify-icon icon="uis:refresh" class="icon text-xl line-height-1"></iconify-icon>
-                    </button>
-                </div>
-
-                @if ($issue->status == "OPEN")
-                    <div class="flex items-center justify-end gap-3">
-                        <button onclick="Livewire.dispatch('showModalAISuggestRootCause', ['{{ $issue->id }}' ,'{{ $issue->line }}', '{{ $issue->description }}'])" class="btn btn-secondary border border-neutral-600 text-sm btn-sm px-3 py-3 rounded-lg flex items-center gap-2">
-                            <iconify-icon icon="mingcute:ai-line" class="icon text-xl line-height-1"></iconify-icon>
-                            Tanya AI
-                        </button>
-                        <button onclick="Livewire.dispatch('showModalFormRootCause', { id: '' , issue_id: '{{ $issue->id }}' })" class="btn btn-primary text-sm btn-sm px-3 py-3 rounded-lg flex items-center gap-2">
-                            <iconify-icon icon="ic:baseline-plus" class="icon text-xl line-height-1"></iconify-icon>
-                            Tambahkan Akar Masalah
-                        </button>
-                    </div>
-                @endif
-            </div>
-            <div class="card-body p-6">
-                <table id="genba-cause-table" class="border border-neutral-200 dark:border-neutral-600 rounded-lg border-separate	">
-                    <thead>
-                        <tr>
-                            <th scope="col" class="text-neutral-800 dark:text-white">
-                                <div class="form-check style-check flex items-center">
-                                    <input class="form-check-input" id="serial" type="checkbox">
-                                    <label class="ms-2 form-check-label" for="serial">
-                                        No.
-                                    </label>
-                                </div>
-                            </th>
-                            <th scope="col" class="text-neutral-800 dark:text-white">
-                                <div class="flex items-center gap-2">
-                                    Oleh
-                                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
-                                    </svg>
-                                </div>
-                            </th>
-                            <th scope="col" class="text-neutral-800 dark:text-white">
-                                <div class="flex items-center gap-2">
-                                    Kategori Penyebab
-                                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
-                                    </svg>
-                                </div>
-                            </th>
-                            <th scope="col" class="text-neutral-800 dark:text-white">
-                                <div class="flex items-center gap-2">
-                                    Deskripsi
-                                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
-                                    </svg>
-                                </div>
-                            </th>
-                            <th scope="col" class="text-neutral-800 dark:text-white">
-                                <div class="flex items-center gap-2">
-                                    Action
-                                </div>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($root_causes as $cause)
-                            <tr>
-                                <td>
-                                    <div class="form-check style-check flex items-center">
-                                        <input class="form-check-input" type="checkbox">
-                                        <label class="ms-2 form-check-label">
-                                            {{ $cause->id }}
-                                        </label>
-                                    </div>
-                                </td>
-                                <td>
-                                    <h6 class="text-base mb-0 font-medium grow"> {{ $cause->creator->name }} </h6>
-                                </td>
-                                <td>
-                                    <p> {{ $cause->category }} </p>
-                                </td>
-                                <td>
-                                    <span> {{ $cause->description }} </span>
-                                </td>
-                                <td>
-                                    <button onclick="Livewire.dispatch('showModalViewRootCause', { id: '{{ $cause->id }}' })" class="w-8 h-8 bg-primary-50 dark:bg-primary-600/10 text-primary-600 dark:text-primary-400 rounded-full inline-flex items-center justify-center">
-                                        <iconify-icon icon="iconamoon:eye-light"></iconify-icon>
-                                    </button>
-                                    @if ($issue->status == "OPEN")
-                                        <a href="{{ route('cause.delete', [$cause->id]) }}" data-confirm-delete="true" class="w-8 h-8 bg-danger-100 dark:bg-danger-600/25 text-danger-600 dark:text-danger-400 rounded-full inline-flex items-center justify-center">
-                                            <iconify-icon icon="mingcute:delete-2-line"></iconify-icon>
-                                        </a>
-
-                                        <button onclick="Livewire.dispatch('showModalFormRootCause', { id: '{{ $cause->id }}' })" class="w-8 h-8 bg-success-100 dark:bg-success-600/25 text-success-600 dark:text-success-400 rounded-full inline-flex items-center justify-center">
-                                            <iconify-icon icon="lucide:edit"></iconify-icon>
-                                        </button>
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        {{-- Root Cause Card : End  --}}
+        @livewire('Card.Table.Issue.RootCause', ["issue" => $issue])
 
         {{-- Solution Card : Start  --}}
         <div class="card h-full p-0 rounded-xl border-0 overflow-hidden w-full my-4">
@@ -530,7 +423,6 @@
     @livewire('Card.AI.Suggest.Action')
 
     <script src="{{ asset('assets/js/defaultCarousel.js') }}"></script>
-    <script src="{{ asset('assets/js/data-table/genba-cause.js') }}"></script>
     <script src="{{ asset('assets/js/data-table/genba-action.js') }}"></script>
 
 
