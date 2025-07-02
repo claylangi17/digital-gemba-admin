b7a985590800d080a512addaecd451a4bf0f10f9
ardfar <farrasarrafi5b@gmail.com>
2025-07-02 14:33:55 +0700

    fix: glitching theme switch button

---
+++ app/Livewire/Switch/ThemeMode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Switch;
+
+use Livewire\Component;
+
+class ThemeMode extends Component
+{
+    public function render()
+    {
+        return view('livewire.switch.theme-mode');
+    }
+}
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,15 +14,7 @@
         </div>
         <div class="col-auto">
             <div class="flex flex-wrap items-center gap-3">
-                <button type="button" id="theme-toggle" class="w-10 h-10 bg-neutral-200 dark:bg-neutral-700 dark:text-white rounded-full flex justify-center items-center">
-                    <span id="theme-toggle-dark-icon" class="hidden">
-                        <i class="ri-sun-line"></i>
-                    </span>
-                    <span id="theme-toggle-light-icon" class="hidden">
-                        <i class="ri-moon-line"></i>
-                    </span>
-                </button>
-
+                @livewire('Switch.ThemeMode')
 
                 {{-- Profile Menu : start  --}}
                 <button data-dropdown-toggle="dropdownProfile" class="flex justify-center items-center rounded-full" type="button">
