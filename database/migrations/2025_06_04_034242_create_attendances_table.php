5c02162203d92355a414c61f9ec39a309c15b907
ardfar <farrasarrafi5b@gmail.com>
2025-07-18 19:19:51 +0700

    fix: removed unused `name` column on the `attendances` table

--- database/migrations/2025_06_04_034242_create_attendances_table.php
+++ database/migrations/2025_06_04_034242_create_attendances_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('session_id')->constrained('genba_sessions')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->string('name');
             $table->enum('status', ["ABSENT", "PRESENT", "LATE"]);
             $table->timestamp('time_in')->nullable();
             $table->timestamp('time_out')->nullable();
